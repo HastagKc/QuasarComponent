099812e9d3cc1e3700793d85d18ed73ef8a57c0b
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-09-04 13:14:20 +0545

    image card

--- src/components/quasarcomponents/CardCom.vue
+++ src/components/quasarcomponents/CardCom.vue
@@ -1,7 +1,97 @@
 <template>
-  <h5>Card</h5>
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <div class="row q-col-gutter-md">
+          <div class="col-3">
+            <!-- First Card -->
+            <q-card class="q-ma-md">
+              <q-card-section>
+                <div class="text-h5 text-dark text-bold">This card Heading</div>
+                <div class="text-subtitle2 text-dark">
+                  This is going to be a subtitle of card
+                </div>
+              </q-card-section>
+              <q-separator inset />
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat impedit deserunt, similique asperiores ducimus nostrum
+                blanditiis quae repudiandae, perferendis esse qui inventore
+                cumque architecto quos natus voluptatum officia, aut beatae.
+              </q-card-section>
+              <q-card-actions vertical>
+                <q-btn label="Watch" flat></q-btn>
+                <q-btn label="Review" flat></q-btn>
+              </q-card-actions>
+            </q-card>
+          </div>
+          <!-- Second Card -->
+          <div class="col-3">
+            <q-card class="q-ma-md bg-amber-4">
+              <q-card-section>
+                <div class="text-h5 text-dark text-bold">This card Heading</div>
+                <div class="text-subtitle2 text-dark">
+                  This is going to be a subtitle of card
+                </div>
+              </q-card-section>
+              <q-separator inset />
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat impedit deserunt, similique asperiores ducimus nostrum
+                blanditiis quae repudiandae, perferendis esse qui inventore
+                cumque architecto quos natus voluptatum officia, aut beatae.
+              </q-card-section>
+              <q-card-actions align="right">
+                <q-btn label="Watch" flat></q-btn>
+                <q-btn label="Review" flat></q-btn>
+              </q-card-actions>
+            </q-card>
+          </div>
+
+          <!-- third card  -->
+
+          <div class="col-3">
+            <q-card class="q-ma-md">
+              <q-card-section class="image__div">
+                <q-img
+                  class="zoom-img"
+                  src="@assets/images/coffeeshop.jpg"
+                ></q-img>
+              </q-card-section>
+              <q-card-section class="q-pa-t-none">
+                <div class="text-h6 text-bold">Coffee Shop</div>
+                <div class="text-subtitle2">Enhance your coffee experience</div>
+              </q-card-section>
+              <q-separator inset />
+              <q-card-actions align="right">
+                <q-btn flat class="text-blue">
+                  <a
+                    href="http://www.youtube.com"
+                    style="text-decoration: none"
+                  >
+                    Visit
+                  </a>
+                </q-btn>
+              </q-card-actions>
+            </q-card>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup></script>
 
-<style></style>
+<style scoped>
+.image__div {
+  overflow: hidden;
+}
+.zoom-img {
+  transition: transform 0.3s ease;
+}
+
+.zoom-img:hover {
+  transform: scale(1.5);
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
@@ -10,5 +11,13 @@ export default defineConfig({
     quasar({
       sassVariables: 'src/quasar-variables.sass'
     })
-  ]
-})
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias for 'src' directory
+      '@components': path.resolve(__dirname, './src/components'), // Alias for 'components' directory
+      '@assets': path.resolve(__dirname, './src/assets'), // Alias for 'assets' directory
+      '@images': path.resolve(__dirname, './src/assets/images'), // Alias for 'images' directory
+    }
+  }
+});
